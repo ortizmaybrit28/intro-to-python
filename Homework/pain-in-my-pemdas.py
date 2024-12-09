c123ffbe0fcb2b8752fde76c9edd8283ab97cd80
Maybrit S. Ortiz <ortiz.maybrit@berkeley.edu>
2024-12-08 21:43:22 -0800

    Homework #2 added under the name questions_maybrit.txt

--- Homework/pain-in-my-pemdas.py
+++ Homework/pain-in-my-pemdas.py
@@ -1,5 +1,6 @@
-# A "heinous" example of arithmetic math problem is the following"
+# A "heinous" example of arithmetic math problem is the following:
 print((5*4*3*2*1)/(3*2*1)*5-12(10-9)/2+1)
+
 # The answer should be 95
 # Seemingly simple arithmetic problem requires PEMDAS
 # I would see problems like this on the internet, it ALWAYS causes a stir
@@ -9,8 +10,8 @@ print((5*4*3*2*1)/(3*2*1)*5-12(10-9)/2+1)
 # Well, PEMDAS can be a pain in the butt! (I don't have the best humor, but that's ok..)	
 
 # What is the process to run this python file?
-# cd intro-to-python/homeworks (This pathway takes me to the directory (homeworks).)
-# do nano "pain-in-my-pemdas.py" (create a python file, and it is named homework-1.py)
+# cd intro-to-python/homework (This pathway takes me to the directory (homework).)
+# do nano "pain-in-my-pemdas.py" (create a python file, and it is named pain-in-my-pemdas.py)
 # Insert comments/print, save the file, and close it to return to the command line
 # python3 pain-in-my-pemdas.py (this runs the python file.)
 
